ce0a251f3ec09142e5693243c6a8142ad797a2d2
VincentColiin <89923106+VincentColiin@users.noreply.github.com>
2021-12-01 18:17:08 +0700

    Mengupdate movement dan menambahkan rotate

--- TerraFic/Assets/Scripts/FollowPath.cs
+++ TerraFic/Assets/Scripts/FollowPath.cs
@@ -2,24 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FollowPath : MonoBehaviour
 {
 
-    [SerializeField] float speed;
-    [SerializeField] Transform[] checkpoint;
-    [SerializeField] Transform car;
+    enum rotate
+    {
+        left,
+        right
+    }
+
+    [SerializeField] float minDist = 0.2f;
+    [SerializeField] float speed=0.1f;
+    [SerializeField] Transform[] points=new Transform[2];
+    [SerializeField] rotate[] rotates;
+    GameObject car;
     private float time;
+    private int i;
+    private bool rotated;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Masih perlu diganti agar bisa dipake untuk semua mobil
-        time =(time + Time.deltaTime*speed)%1f;
-        car.position = Vector3.Lerp(checkpoint[0].position, checkpoint[1].position, time);
+        if ((transform.position - points[i+1].position).magnitude >= minDist)
+        {
+            time = (time + Time.deltaTime * speed);
+            transform.position = Vector3.Lerp(points[i].position, points[i + 1].position, time);
+
+            if (i>0 && !rotated)
+            {
+                switch (rotates[i-1])
+                {
+                    case rotate.left:
+                        transform.rotation *= Quaternion.Euler(0, 0, 90);
+                        rotated = true;
+                        break;
+                    case rotate.right:
+                        transform.rotation *= Quaternion.Euler(0, 0, -90);
+                        rotated = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } else if(i<points.Length-2)
+        {
+            i++;
+            rotated = false;
+            time = 0;
+        }
     }
 }
